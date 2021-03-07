50b7becbb2986a1a965f9ab49cd539c8a0f8f202
Mat <mvello66@gmail.com>
2021-03-06 21:52:59 -0500

    model methods

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,9 @@
 class Card < ApplicationRecord
+  def rarity_info
+    if rarity != " "
+      return "this card has a rarity of #{rarity} cuz i said so"
+    else
+      return "no rarity"
+    end
+  end
 end
