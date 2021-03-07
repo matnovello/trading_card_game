6aaac9a81e88648b373c70fde9c9df7772cf3c15
Mat <mvello66@gmail.com>
2021-03-06 20:51:10 -0500

    adds RESTful routes, card partial

--- app/controllers/api/trading_cards_controller.rb
+++ app/controllers/api/trading_cards_controller.rb
@@ -1,5 +1,6 @@
 class Api::TradingCardsController < ApplicationController
   def index
+    @cards = Card.all
     render "index.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,16 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+    #index
+    get "/trading_cards" => "trading_cards#index"
+    #show
+    get "/trading_cards/:id" => "trading_cards#show"
+    #create
+    post "/trading_cards" => "trading_cards#create"
+    #patch
+    post "/trading_cards" => "trading_cards#patch"
+    #delete
+    get "/trading_cards/:id" => "trading_cards#destroy"
+  end
 end
